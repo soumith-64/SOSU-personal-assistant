6853e4465c8543c31cf64cdf267d7057faa983b5
Soumith <141291462+soumith-64@users.noreply.github.com>
2026-07-29 23:23:49 +0530

    feat: startup and parser feature added

--- core/parser.py
+++ core/parser.py
@@ -1,8 +1,14 @@
 from utils import logger
 
+def check_inp(val):
+    usr_inp_sep = val.strip().split(" ")
+
+    if len(usr_inp_sep) == 1:
+        command = usr_inp_sep[0]
+    elif len(usr_inp_sep) >= 2:
+        command,argument = usr_inp_sep[0],usr_inp_sep[1]
+    print(command+"\n"+argument)
 
 def split_imput(user_input):
-
-    usr_inp_sep = user_input.strip().split(" ")
-    command,argument = usr_inp_sep[0],usr_inp_sep[1]
-    print(command+"\n"+argument)
+    logger.info(f"User : {user_input}")
+    check_inp(user_input)
