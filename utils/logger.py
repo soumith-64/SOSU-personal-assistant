2902a198489a893df8f4b9804ceb9bb5cffd4e3d
Soumith <141291462+soumith-64@users.noreply.github.com>
2026-07-29 17:30:04 +0530

    feat: updated logger

--- utils/logger.py
+++ utils/logger.py
@@ -1,26 +1,42 @@
-from config import BASE_DIR,APP_LOG_FILE
-import time,os
+import os
+import time
+from config import APP_LOG_FILE, BASE_DIR
 
-time_stamp = time.strftime("%Y-%m-%d %H:%M:%S")
+
+def timestamp() -> str:
+    return time.strftime("%Y-%m-%d %H:%M:%S")
 
 
-def write_log(sentence):
+def sent_create(message: str, type_info):
+    time_stamp = timestamp()
+    return f"[{time_stamp}] [{type_info.upper()}] [{message}]"
 
-    os.makedirs(APP_LOG_FILE , exist_ok=True)
 
-    with open(APP_LOG_FILE,"a") as logging:
+def write_log(sentence: str) -> None:
+    os.makedirs(os.path.dirname(APP_LOG_FILE), exist_ok=True)
+
+    with open(APP_LOG_FILE, "a", encoding="utf-8") as logging:
         logging.write(f"{sentence}\n")
 
-def info(message):
 
-    sent = f"[{time_stamp}] Info [{message}] "
-    write_log(sent)
+def check_val(val):
+
+    if val and val.strip():
+        write_log(val)
+    else:
+        raise RuntimeError("Logger error: Log message cannot be empty.")
+
+def info(message):
+    sent = sent_create(message, "Info")
+    check_val(sent)
+
 
 def warning(message):
-    sent = f"[{time_stamp}] Info [{message}] "
-    write_log(sent)
+    sent = sent_create(message, "Warning")
+    check_val(sent)
+
 
 def error(message):
-    sent = f"[{time_stamp}] Info [{message}] "
-    write_log(sent)
+    sent = sent_create(message, "Error")
+    check_val(sent)
     
