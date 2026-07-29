e734ca767e9c492481f68bd8d61c770087191060
Soumith <141291462+soumith-64@users.noreply.github.com>
2026-07-29 17:11:12 +0530

    feat: updated logger

--- utils/logger.py
+++ utils/logger.py
@@ -1,7 +1,26 @@
 from config import BASE_DIR,APP_LOG_FILE
+import time,os
 
-def write_log(level, message):
+time_stamp = time.strftime("%Y-%m-%d %H:%M:%S")
+
+
+def write_log(sentence):
+
+    os.makedirs(APP_LOG_FILE , exist_ok=True)
 
     with open(APP_LOG_FILE,"a") as logging:
-        pass
+        logging.write(f"{sentence}\n")
+
+def info(message):
+
+    sent = f"[{time_stamp}] Info [{message}] "
+    write_log(sent)
+
+def warning(message):
+    sent = f"[{time_stamp}] Info [{message}] "
+    write_log(sent)
+
+def error(message):
+    sent = f"[{time_stamp}] Info [{message}] "
+    write_log(sent)
     
