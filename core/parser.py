eb98d7f33064c9c05434dfd3d1f6de9d82aed367
Soumith <141291462+soumith-64@users.noreply.github.com>
2026-07-29 23:34:00 +0530

    feat: startup and parser feature added

--- core/parser.py
+++ core/parser.py
@@ -1,14 +1,25 @@
+from config import PROMPT
 from utils import logger
 
+
 def check_inp(val):
-    usr_inp_sep = val.strip().split(" ")
+    
+    if not val or not val.strip():
+        logger.warning("Empty user input received.")
+        print(f"{PROMPT} Hi, I'm waiting to hear from you.")
+        return
+
+    usr_inp_sep = val.strip().split()
 
     if len(usr_inp_sep) == 1:
         command = usr_inp_sep[0]
-    elif len(usr_inp_sep) >= 2:
-        command,argument = usr_inp_sep[0],usr_inp_sep[1]
-    print(command+"\n"+argument)
+        print(f"Command: {command}")
+    else:
+        command = usr_inp_sep[0]
+        argument = " ".join(usr_inp_sep[1:])
+        print(f"Command: {command}\nArgument: {argument}")
 
-def split_imput(user_input):
-    logger.info(f"User : {user_input}")
+
+def split_input(user_input):
+    logger.info(f"User: {user_input}")
     check_inp(user_input)
