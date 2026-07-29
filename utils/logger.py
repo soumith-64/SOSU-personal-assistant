da601892f214a29a94a3ddea39758e7f363c66a0
Soumith <141291462+soumith-64@users.noreply.github.com>
2026-07-29 21:58:51 +0530

    feat: updated logger

--- utils/logger.py
+++ utils/logger.py
@@ -1,42 +1,17 @@
 import os
 import time
-from config import APP_LOG_FILE, BASE_DIR
+from config import APP_LOG_FILE
 
 
-def timestamp() -> str:
+def timestamp():
     return time.strftime("%Y-%m-%d %H:%M:%S")
 
 
-def sent_create(message: str, type_info):
+def format_log(message, level):
     time_stamp = timestamp()
-    return f"[{time_stamp}] [{type_info.upper()}] [{message}]"
+    return f"[{time_stamp}] [{level.upper()}] [{message}]"
 
 
-def write_log(sentence: str) -> None:
-    os.makedirs(os.path.dirname(APP_LOG_FILE), exist_ok=True)
-
-    with open(APP_LOG_FILE, "a", encoding="utf-8") as logging:
-        logging.write(f"{sentence}\n")
 
 
-def check_val(val):
-
-    if val and val.strip():
-        write_log(val)
-    else:
-        raise RuntimeError("Logger error: Log message cannot be empty.")
-
-def info(message):
-    sent = sent_create(message, "Info")
-    check_val(sent)
-
-
-def warning(message):
-    sent = sent_create(message, "Warning")
-    check_val(sent)
-
-
-def error(message):
-    sent = sent_create(message, "Error")
-    check_val(sent)
     
