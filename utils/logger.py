793dbc3c79dbe3c2635fadbf1d201d733bdce6cb
Soumith <141291462+soumith-64@users.noreply.github.com>
2026-07-29 21:59:08 +0530

    feat: updated logger

--- utils/logger.py
+++ utils/logger.py
@@ -11,6 +11,16 @@ def format_log(message, level):
     time_stamp = timestamp()
     return f"[{time_stamp}] [{level.upper()}] [{message}]"
 
+def write_log(sentence):
+
+    if sentence and sentence.strip():
+        os.makedirs(os.path.dirname(APP_LOG_FILE), exist_ok=True)
+        
+        with open(APP_LOG_FILE, "a", encoding="utf-8") as logging:
+            logging.write(f"{sentence}\n")
+    else:
+        raise ValueError("Log message cannot be empty.")
+
 
 
 
