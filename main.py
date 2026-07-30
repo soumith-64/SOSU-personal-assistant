065e87d16df728c44c96d339903d6f28e1b936d8
Soumith <141291462+soumith-64@users.noreply.github.com>
2026-07-30 22:49:26 +0530

    feat: router  feature added

--- main.py
+++ main.py
@@ -4,6 +4,22 @@ startup.start()
 
 assistant.run()
 
-git add .
-git commit -m "feat: router  feature added"
-git push   
+from core import parser
+from config import PROMPT
+from utils import logger
+
+def run():
+    while True:
+
+        try :
+            input_user = input("User : ")
+            if input_user.strip():
+                result = parser.parse_input(input_user)
+                break
+
+            print(f"{PROMPT} Hi, I'm waiting to hear from you.")
+            logger.warning("Empty user input received.")
+        except KeyboardInterrupt:
+            print("Error Keyboard Interrupted")
+            logger.error("KeyboardInterrupt")
+            break
