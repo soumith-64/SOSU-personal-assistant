74a4aab801f1a975a3364d36722675ade816323b
Soumith <141291462+soumith-64@users.noreply.github.com>
2026-07-30 22:48:36 +0530

    feat: router  feature added

--- main.py
+++ main.py
@@ -4,18 +4,3 @@ startup.start()
 
 assistant.run()
 
-
-from core import parser
-from config import PROMPT
-from utils import logger
-
-def run():
-    while True:
-
-        try :
-            input_user = input("User : ")
-            if input_user.strip():
-                result = parser.parse_input(input_user)
-                break
-
-            print(f"{PROMPT} Hi, I'm waiting to hear from you.")
